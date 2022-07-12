c275bf4bdeb73aea7ed1da73ffc845611c730ca4
godtheit <arne23@hotmail.de>
2022-07-12 15:19:47 +0200

    New weight matrix

--- Analyse_Simulation/Analyse.R
+++ Analyse_Simulation/Analyse.R
@@ -32,6 +32,24 @@ th33 <- c(0,0,1,0,0,1,1,1,0) #9
 w <- rbind(th11,th12, th13, th21, th22, th23, th31, th32, th33)
 colnames(w) <- rownames(w)
 
+a11 <- c(0,0,0,0,0,0,0,0,0) #1
+a12 <- c(8,0,0,0,0,0,0,0,0) #2
+a13 <- c(6,8,0,0,0,0,0,0,0) #3
+a21 <- c(8,6,4,0,0,0,0,0,0) #4
+a22 <- c(6,8,6,8,0,0,0,0,0) #5
+a23 <- c(4,6,8,6,8,0,0,0,0) #6
+a31 <- c(6,4,2,8,6,4,0,0,0) #7
+a32 <- c(4,6,4,6,8,6,8,0,0) #8
+a33 <- c(2,4,6,4,6,8,6,8,0) #9
+
+w_distance2 <- rbind(a11,a12, a13, a21, a22, a23, a31, a32, a33)
+w_distance <- w_distance2/10
+colnames(w_distance) <- rownames(w_distance)
+
+
+g <- matrix(1 , 9, 9)
+g[which(upper.tri(w, diag = TRUE)==TRUE)] <- 0
+
 lam1 <- seq(0.1,2, length.out = 20)
 lam2 <- seq(0.01,10, length.out = 8)
 
@@ -57,5 +75,20 @@ List__Estimations_Mds[[2]]  <- procedure(Y = current_network, lam1 = lam1,
 }
 #hurray
 
+vergleich1 <- MdS(Y = current_network, lambda1 = 1, lambda2 = 1, w = w)
+vergleich2 <- JGL(Y = current_network, lambda1 = 1, lambda2 = 1, penalty = "fused",
+                  return.whole.theta = T)
+
+data(example.data)
+str(example.data)
+## run FGL:
+fgl.results = JGL(Y=example.data,penalty="fused",lambda1=3,lambda2=10)
+str(fgl.results)
+print.jgl(fgl.results)
 
 
+
+u <- matrix(1 , 2, 2)
+u[which(upper.tri(u, diag = TRUE)==TRUE)] <- 0
+
+MdS.results = MdS(Y = example.data, lambda1 = 0.26, lambda2 = 0.1, w = u)
