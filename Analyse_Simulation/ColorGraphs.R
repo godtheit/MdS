a7344bc9926d0062e139f8c586ed62b89e30055d
godtheit <arne23@hotmail.de>
2022-08-01 13:27:07 +0200

    Colored Graphs

--- Analyse_Simulation/ColorGraphs.R
+++ Analyse_Simulation/ColorGraphs.R
@@ -1,5 +1,5 @@
-setwd("H:/Documents/MdS/Daten_der_Analyse")
-
+#setwd("H:/Documents/MdS/Daten_der_Analyse")
+setwd("C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse")
 library(igraph)
 library(visNetwork)
 library(gridExtra)
@@ -15,7 +15,7 @@ add_all_adjs <- matrix(0,134,134)
 shared_network <- matrix(0,134,134)
 for (m in 1:9) {
   graphs_MdS[[m]] <- graph_from_adjacency_matrix(Adjs_MdS[[m]], mode= "undirected")
-  V(graphs_MdS[[m]])$name <- 1:134
+  V(graphs_MdS[[m]])$name <- as.character(1:134)
 
   add_all_adjs <- add_all_adjs + Adjs_MdS[[m]]
 
@@ -32,6 +32,7 @@ nicht_null  <- which((degree(graphs_MdS[[1]]) != 0) | (degree(graphs_MdS[[2]]) !
 
 for (m in 1:9) {
   subgraphs[[m]] <- induced.subgraph(graphs_MdS[[m]], vids = nicht_null)
+  E(subgraphs[[m]])$color <- "red"
 }
 shared_network[which(add_all_adjs == 9)] <- 1
 
@@ -41,31 +42,254 @@ shared_network2 <- intersection(graphs_MdS[[1]], graphs_MdS[[2]], graphs_MdS[[3]
 
 graphs_shared <- graph_from_adjacency_matrix(shared_network, mode= "undirected")
 
+shared <- induced.subgraph(shared_network2, vids = nicht_null)
+E(shared)$color <- "blue"
 
-
-Edges_gemeinsam <- E(shared_network2)
-
-
-
-
+graphs <- list()
 for (m in 1:9) {
-  for (k in 1:length(E(subgraphs[[m]])) ) {
-    if(E(subgraphs[[m]])[k] %in% Edges_gemeinsam == TRUE){
-      E(subgraphs[[m]])$color[k] <- 'blue'
-    } else if (E(subgraphs[[m]])[k] %in% Edges_gemeinsam == FALSE) {
-      cat(k, "   ")
-      E(subgraphs[[m]])$color[k] <- 'red'
-      }
+  graphs[[m]] <- union(shared,subgraphs[[m]])
+
+  for (i in 1:length(E(graphs[[m]]))) {
+    if (is.na(E(graphs[[m]])$color_1[i]) == TRUE) {
+      E(graphs[[m]])$color[i] <- "red"
+    } else { E(graphs[[m]])$color[i] <- "blue"}
+
   }
 }
 
+Gruppe <- c("GruppeA", "GruppeB", "GruppeC")
+Strahlung <- c("0gy", "0.05gy", "2gy")
 
-plot(subgraphs[[1]], layout = layout_on_grid ,vertex.size = 0.1)
-legend("topright", legend=c("Kernnetzwerk", "Andere"),
-       col=c("red", "blue"), lty=1, cex=0.8,
-       box.lty=0)
-title("GruppeB, 0gy")
 
-Edges_gemeinsam[33]
-E(subgraphs[[1]])[143]
+# path <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Graphs/Graphs_mit_Farbe"
+# counter <- 1
+# for (i in 1:3) {
+#   for (j in 1:3) {
+#     png(file=paste(path, "/Theta", i,j, ".png", sep = ""), width= 960, height = 960, res = 120)
+#     plot(graphs[[counter]], layout = layout_on_grid ,vertex.size = 0.1)
+#     legend("topright", legend=c("Kernnetzwerk", "Änderung"),
+#            col=c("blue", "red"), lty=1, cex=0.8,
+#            box.lty=0)
+#     title(paste(Gruppe[j],Strahlung[i]))
+#     dev.off()
+#     counter <- counter + 1
+#   }
+# }
 
+
+
+library(bnstruct)
+#bnstruct::shd(as.matrix(Adjs_MdS[[1]]), as.matrix())
+#bnstruct::shd(Adjs_MdS[[1]], Adjs_MdS[[3]])
+
+diff_matrix <- matrix(0, nrow=9, ncol = 9)
+
+for (i in 1:9) {
+  for (j in 1:9) {
+    diff_matrix[i,j] <- bnstruct::shd(Adjs_MdS[[i]], Adjs_MdS[[j]])
+    diff_matrix[j,i] <- diff_matrix[i,j]
+  }
+}
+colnames(diff_matrix) <- rownames(diff_matrix) <- c("GrpA_0gy", "GrpB_0gy", "GrpC_0gy",
+                                                    "GrpA_0.05gy", "GrpB_0.05gy", "GrpC_0.05gy",
+                                                    "GrpA_2gy", "GrpB_2gy", "GrpC_2gy")
+heatmap(diff_matrix)
+library(reshape2)
+melted_cormat <- melt(diff_matrix)
+head(melted_cormat)
+
+library(ggplot2)
+ggplot(data = melted_cormat, aes(x=Var2, y=Var1, fill=value)) +
+  geom_tile()
+
+diffmatrix2 <- diff_matrix/100
+
+ggcorrplot::ggcorrplot(diffmatrix2)
+heatmap(diff_matrix)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################### Unterschiede pro Gruppe
+
+
+
+
+Subgraphs_0gy <- list(subgraphs[[1]], subgraphs[[2]], subgraphs[[3]])
+Subgraphs_005gy <- list(subgraphs[[4]], subgraphs[[5]], subgraphs[[6]])
+Subgraphs_2gy <- list(subgraphs[[7]], subgraphs[[8]], subgraphs[[9]])
+
+Subgraphs_GrpA <- list(subgraphs[[1]], subgraphs[[4]], subgraphs[[7]])
+Subgraphs_GrpB <- list(subgraphs[[2]], subgraphs[[5]], subgraphs[[8]])
+Subgraphs_GrpC <- list(subgraphs[[3]], subgraphs[[6]], subgraphs[[9]])
+
+
+
+###################### 0 gy
+final_0gy <- list()
+diff_0gy <- list()
+shared_network_0gy <- intersection(Subgraphs_0gy[[1]],Subgraphs_0gy[[2]],Subgraphs_0gy[[3]], keep.all.vertices = FALSE)
+for (m in 1:3) {
+  diff_0gy[[m]] <- difference(Subgraphs_0gy[[m]], shared_network_0gy)
+}
+
+
+nicht_null_0gy <- which((degree(diff_0gy[[1]]) != 0) | (degree(diff_0gy[[2]]) != 0) |
+                          (degree(diff_0gy[[3]]) != 0))
+
+for (m in 1:3) {
+final_0gy[[m]] <- induced.subgraph(diff_0gy[[m]], vids = nicht_null_0gy)
+E(final_0gy[[m]])$color <- "blue"
+}
+
+path <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Graphs/Graphs_mit_Farbe/Ohne0gy"
+
+for (j in 1:3) {
+    png(file=paste(path, "/Theta", 1,j, ".png", sep = ""), width= 960, height = 960, res = 120)
+    plot(final_0gy[[j]], layout = layout_on_grid ,vertex.size = 0.1)
+    dev.off()
+
+
+}
+
+###################### 0.05 gy
+final_005gy <- list()
+diff_005gy <- list()
+shared_network_005gy <- intersection(Subgraphs_005gy[[1]],Subgraphs_005gy[[2]],Subgraphs_005gy[[3]], keep.all.vertices = FALSE)
+for (m in 1:3) {
+  diff_005gy[[m]] <- difference(Subgraphs_005gy[[m]], shared_network_005gy)
+}
+
+
+nicht_null_005gy <- which((degree(diff_005gy[[1]]) != 0) | (degree(diff_005gy[[2]]) != 0) |
+                          (degree(diff_005gy[[3]]) != 0))
+
+for (m in 1:3) {
+  final_005gy[[m]] <- induced.subgraph(diff_005gy[[m]], vids = nicht_null_005gy)
+  E(final_005gy[[m]])$color <- "blue"
+}
+
+path <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Graphs/Graphs_mit_Farbe/Ohne005gy"
+
+for (j in 1:3) {
+  png(file=paste(path, "/Theta", 2,j, ".png", sep = ""), width= 960, height = 960, res = 120)
+  plot(final_005gy[[j]], layout = layout_on_grid ,vertex.size = 0.1)
+  dev.off()
+
+
+}
+
+###################### 2 gy
+final_2gy <- list()
+diff_2gy <- list()
+shared_network_2gy <- intersection(Subgraphs_2gy[[1]],Subgraphs_2gy[[2]],Subgraphs_2gy[[3]], keep.all.vertices = FALSE)
+for (m in 1:3) {
+  diff_2gy[[m]] <- difference(Subgraphs_2gy[[m]], shared_network_2gy)
+}
+
+
+nicht_null_2gy <- which((degree(diff_2gy[[1]]) != 0) | (degree(diff_2gy[[2]]) != 0) |
+                          (degree(diff_2gy[[3]]) != 0))
+
+for (m in 1:3) {
+  final_2gy[[m]] <- induced.subgraph(diff_2gy[[m]], vids = nicht_null_2gy)
+  E(final_2gy[[m]])$color <- "blue"
+}
+
+path <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Graphs/Graphs_mit_Farbe/Ohne2gy"
+
+for (j in 1:3) {
+  png(file=paste(path, "/Theta", 3,j, ".png", sep = ""), width= 960, height = 960, res = 120)
+  plot(final_2gy[[j]], layout = layout_on_grid ,vertex.size = 0.1)
+  dev.off()
+
+
+}
+###################### GruppeA
+final_GrpA <- list()
+diff_GrpA <- list()
+shared_network_GrpA <- intersection(Subgraphs_GrpA[[1]],Subgraphs_GrpA[[2]],Subgraphs_GrpA[[3]], keep.all.vertices = FALSE)
+for (m in 1:3) {
+  diff_GrpA[[m]] <- difference(Subgraphs_GrpA[[m]], shared_network_GrpA)
+}
+
+
+nicht_null_GrpA <- which((degree(diff_GrpA[[1]]) != 0) | (degree(diff_GrpA[[2]]) != 0) |
+                          (degree(diff_GrpA[[3]]) != 0))
+
+for (m in 1:3) {
+  final_GrpA[[m]] <- induced.subgraph(diff_GrpA[[m]], vids = nicht_null_GrpA)
+  E(final_GrpA[[m]])$color <- "blue"
+}
+
+path <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Graphs/Graphs_mit_Farbe/OhneGruppeA"
+
+for (j in 1:3) {
+  png(file=paste(path, "/Theta", j,1, ".png", sep = ""), width= 960, height = 960, res = 120)
+  plot(final_GrpA[[j]], layout = layout_on_grid ,vertex.size = 0.1)
+  dev.off()
+
+
+}
+###################### GruppeB
+final_GrpB <- list()
+diff_GrpB <- list()
+shared_network_GrpB <- intersection(Subgraphs_GrpB[[1]],Subgraphs_GrpB[[2]],Subgraphs_GrpB[[3]], keep.all.vertices = FALSE)
+for (m in 1:3) {
+  diff_GrpB[[m]] <- difference(Subgraphs_GrpB[[m]], shared_network_GrpB)
+}
+
+
+nicht_null_GrpB <- which((degree(diff_GrpB[[1]]) != 0) | (degree(diff_GrpB[[2]]) != 0) |
+                           (degree(diff_GrpB[[3]]) != 0))
+
+for (m in 1:3) {
+  final_GrpB[[m]] <- induced.subgraph(diff_GrpB[[m]], vids = nicht_null_GrpB)
+  E(final_GrpB[[m]])$color <- "blue"
+}
+
+path <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Graphs/Graphs_mit_Farbe/OhneGruppeB"
+
+for (j in 1:3) {
+  png(file=paste(path, "/Theta", j,2, ".png", sep = ""), width= 960, height = 960, res = 120)
+  plot(final_GrpB[[j]], layout = layout_on_grid ,vertex.size = 0.1)
+  dev.off()
+
+
+}
+
+###################### GruppeC
+final_GrpC <- list()
+diff_GrpC <- list()
+shared_network_GrpC <- intersection(Subgraphs_GrpC[[1]],Subgraphs_GrpC[[2]],Subgraphs_GrpC[[3]], keep.all.vertices = FALSE)
+for (m in 1:3) {
+  diff_GrpC[[m]] <- difference(Subgraphs_GrpC[[m]], shared_network_GrpC)
+}
+
+
+nicht_null_GrpC <- which((degree(diff_GrpC[[1]]) != 0) | (degree(diff_GrpC[[2]]) != 0) |
+                           (degree(diff_GrpC[[3]]) != 0))
+
+for (m in 1:3) {
+  final_GrpC[[m]] <- induced.subgraph(diff_GrpC[[m]], vids = nicht_null_GrpC)
+  E(final_GrpC[[m]])$color <- "blue"
+}
+
+path <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Graphs/Graphs_mit_Farbe/OhneGruppeC"
+
+for (j in 1:3) {
+  png(file=paste(path, "/Theta", j,3, ".png", sep = ""), width= 960, height = 960, res = 120)
+  plot(final_GrpC[[j]], layout = layout_on_grid ,vertex.size = 0.1)
+  dev.off()
+
+
+}
--- Analyse_Simulation/PlottingGenResults.R
+++ Analyse_Simulation/PlottingGenResults.R
@@ -1,6 +1,7 @@
-#setwd("C:/Users/arne2/Dropbox/Masterarbeit/MdS/Analyse_Simulation")
+setwd("C:/Users/arne2/Dropbox/Masterarbeit/MdS/Daten_der_Analyse")
+#setwd("C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse")
 
-setwd("H:/Documents/MdS/Daten_der_Analyse")
+#setwd("H:/Documents/MdS/Daten_der_Analyse")
 
 library(igraph)
 library(visNetwork)
@@ -24,6 +25,8 @@ graphs_MdS_sub <- list()
 graphs_JGL <- list()
 graphs_JGL_sub <- list()
 
+missing_JGL <- c()
+missing_MdS <- c()
 for (m in 1:9) {
   Adjs_Glasso[[m]] <- Gen_network_Glasso[[m]][[1]]
   Adjs_JGL[[m]] <- Gen_network_JGL[[1]]$theta[[m]]
@@ -39,29 +42,65 @@ for (m in 1:9) {
 
 for (m in 1:9) {
   graphs_Glasso[[m]] <- graph_from_adjacency_matrix(Adjs_Glasso[[m]],mode = "undirected")
+
   graphs_JGL[[m]] <- graph_from_adjacency_matrix(Adjs_JGL[[m]],mode = "undirected")
-
-
-  graphs_MdS[[m]] <- graph_from_adjacency_matrix(Adjs_MdS[[m]], mode= "undirected")
-
-  V(graphs_MdS[[m]])$name <- 1:134
-
   V(graphs_JGL[[m]])$name <- as.character(1:134)
   nix2 <- which(degree(graphs_JGL[[m]]) != 0)
   graphs_JGL_sub[[m]] <- induced.subgraph(graphs_JGL[[m]],
-                                      vids = nix2)
+                                          vids = nix2)
 
-V(graphs_JGL[[m]])$name <- as.character(1:134)
+
+  graphs_MdS[[m]] <- graph_from_adjacency_matrix(Adjs_MdS[[m]], mode= "undirected")
+  V(graphs_MdS[[m]])$name <- 1:134
+
   nix <- which(degree(graphs_MdS[[m]]) != 0)
   graphs_MdS_sub[[m]] <- induced.subgraph(graphs_MdS[[m]],
                                           vids = nix)
+
+  missing_MdS[m] <- 134 - length(V(graphs_MdS_sub[[m]]))
+missing_JGL[m] <- 134 - length(V(graphs_JGL_sub[[m]]))
+
+
+
+
 }
 
 
 
+
+library(bnstruct)
+bnstruct::shd(as.matrix(Adjs_MdS[[1]]), as.matrix())
+bnstruct::shd(Adjs_MdS[[1]], Adjs_MdS[[3]])
+
+diff_matrix <- matrix(0, nrow=9, ncol = 9)
+
+for (i in 1:9) {
+  for (j in 1:9) {
+    diff_matrix[i,j] <- bnstruct::shd(Adjs_MdS[[i]], Adjs_MdS[[j]])
+    diff_matrix[j,i] <- diff_matrix[i,j]
+  }
+}
+
+heatmap(diff_matrix)
+library(reshape2)
+melted_cormat <- melt(diff_matrix)
+head(melted_cormat)
+
+library(ggplot2)
+ggplot(data = melted_cormat, aes(x=Var2, y=Var1, fill=value)) +
+  geom_tile()
+
+diffmatrix2 <- diff_matrix/100
+
+ggcorrplot::ggcorrplot(diffmatrix2)
+heatmap(diff_matrix)
+
 ######################### Graph Plots
 
-path3 <- "H:/Documents/MdS/Daten_der_Analyse/Graphs/Graphs_JGL"
+#path3 <- "H:/Documents/MdS/Daten_der_Analyse/Graphs/Graphs_JGL"
+#path3 <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Graphs/Graphs_Final2"
+path3 <- "C:/Users/arne2/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Graphs/Graphs_Final2"
+
 counter <- 1
 for (i in 1:3) {
   for (j in 1:3) {
@@ -86,8 +125,8 @@ for (i in 1:3) {
 #
 # visSave(network1, file = paste(path3, "/Theta_FULL_",i,j , ".html", sep = ""))
 # visSave(network2, file = paste(path3, "/Theta_Subnetwork_",i,j , ".html", sep = ""))
-g <- graphs_JGL[[counter]]
-g2 <- graphs_JGL_sub[[counter]]
+g <- graphs_MdS[[counter]]
+g2 <- graphs_MdS_sub[[counter]]
 max_deg <- max(degree(g)) # with a length of X vertices :
 max_deg2 <- max(degree(g2))
 colors <- (max_deg+20):20
@@ -104,18 +143,77 @@ for (n in 1:length(V(g2))) {
 
 png(file=paste(path3, "/Theta_",i,j , ".png", sep = ""), width= 960, height = 960, res = 120)
 plot(g, layout=layout_with_fr,vertex.label.dist=0.85, vertex.label.degree=pi/2, vertex.size=5)
+title(paste0("Theta_", i,j))
 dev.off()
 
 
 png(file=paste(path3, "/Theta_SUB_",i,j , ".png", sep = ""), width= 960, height = 960, res = 120)
-plot(g2, layout=  layout_on_grid,vertex.label.dist=0.85, vertex.label.degree=pi/2,vertex.size = 5)
+plot(g2, layout=  layout_on_grid,vertex.label.dist=0.85, vertex.label.degree=pi/2,vertex.size = 5, legend = missing_MdS[counter])
+title(paste0("Theta_",i,j))
+legend('topright', legend = paste0("Fehlende Knoten = ",missing_MdS[counter]),  box.lty = 0)
 dev.off()
 
 
     counter <- counter + 1
   }
 }
-layout_as_tree
+
+path3 <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Graphs/Graphs_JGL"
+counter <- 1
+for (i in 1:3) {
+  for (j in 1:3) {
+
+    #
+    # data <- toVisNetworkData(graphs_MdS[[counter]])
+    # data$nodes$label <- as.character(data$nodes$label)
+    # network1 <- visNetwork(nodes = data$nodes, edges = data$edges)%>%
+    #   #visIgraphLayout(layout = "layout_in_circle") %>%
+    #   #visIgraphLayout(layout = "layout_with_sugiyama") %>%
+    #   visOptions(highlightNearest = list(enabled = TRUE, degree = 1),
+    #              width = "100%", height = "100%")
+    #
+    # data2 <- toVisNetworkData(graphs_MdS_sub[[counter]])
+    # data2$nodes$label <- as.character(data2$nodes$label)
+    # network2 <- visNetwork(nodes = data2$nodes, edges = data2$edges)%>%
+    #   #visIgraphLayout(layout = "layout_in_circle") %>%
+    #   #visIgraphLayout(layout = "layout_with_sugiyama") %>%
+    #   visOptions(highlightNearest = list(enabled = TRUE, degree = 1),
+    #               width = "100%", height = "100%")
+    #
+    #
+    # visSave(network1, file = paste(path3, "/Theta_FULL_",i,j , ".html", sep = ""))
+    # visSave(network2, file = paste(path3, "/Theta_Subnetwork_",i,j , ".html", sep = ""))
+    g <- graphs_JGL[[counter]]
+    g2 <- graphs_JGL_sub[[counter]]
+    max_deg <- max(degree(g)) # with a length of X vertices :
+    max_deg2 <- max(degree(g2))
+    colors <- (max_deg+20):20
+
+    gray_colors <- as.data.frame(paste0("gray", colors))
+    colnames(gray_colors)  <- "farbe"
+
+    for (n in 1:length(V(g))) {
+      V(g)[n]$color <- gray_colors[degree(g)[n] + 1,]
+    }
+    for (n in 1:length(V(g2))) {
+      V(g2)[n]$color <- gray_colors[degree(g2)[n] + 1,]
+    }
+
+    png(file=paste(path3, "/Theta_",i,j , ".png", sep = ""), width= 960, height = 960, res = 120)
+    plot(g, layout=layout_with_fr,vertex.label.dist=0.85, vertex.label.degree=pi/2, vertex.size=5)
+    dev.off()
+
+
+    png(file=paste(path3, "/Theta_SUB_",i,j , ".png", sep = ""), width= 960, height = 960, res = 120)
+    plot(g2, layout=  layout_on_grid,vertex.label.dist=0.85, vertex.label.degree=pi/2,vertex.size = 5, legend = missing_MdS[counter])
+    title(paste0("Theta_",i,j))
+    legend('topright', legend = paste0("Fehlende Knoten = ",missing_JGL[counter]),  box.lty = 0)
+    dev.off()
+
+
+    counter <- counter + 1
+  }
+}
 
 
 
@@ -136,19 +234,48 @@ layout_as_tree
 
 
 ####################### plot tables
-path2 <- "H:/Documents/MdS/Daten_der_Analyse/Tables/Tables_Final2"
+#path2 <- "H:/Documents/MdS/Daten_der_Analyse/Tables/Tables_Final2"
+path2 <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Tables/Tables_Final2"
 counter <- 1
 for (i in 1:3) {
   for (j in 1:3) {
     degrees <- degree(graphs_MdS[[counter]])
-    sorted <- as.data.frame(sort(degrees, decreasing = TRUE))
 
+    sorted2 <- as.data.frame(sort(degrees, decreasing = TRUE))
+    names(sorted2) <- "Anzahl Kanten"
+    sorted2$Id <- rownames(sorted2)
 
-
-    names(sorted) <- "Anzahl Knoten"
+sorted <- sorted2[,c(2,1)]
+rownames(sorted) <- NULL
 x <- head(sorted, n = 12)
 
-    png(file=paste(path2, "/Theta_",i,j , ".png", sep = ""))
+    png(file=paste(path2, "/Theta_",i,j , ".png", sep = ""),height = 25*nrow(x), width = 100*ncol(x))
+    #png(file = paste(path,"table_for_graph",i,j, ".png" sep = ""), height=6, width=4)
+    grid.table(x)
+    dev.off()
+
+
+
+    counter <- counter + 1
+  }
+}
+
+
+path2 <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Tables/Tables_JGL"
+counter <- 1
+for (i in 1:3) {
+  for (j in 1:3) {
+    degrees <- degree(graphs_JGL[[counter]])
+
+    sorted2 <- as.data.frame(sort(degrees, decreasing = TRUE))
+    names(sorted2) <- "Anzahl Kanten"
+    sorted2$Id <- rownames(sorted2)
+
+    sorted <- sorted2[,c(2,1)]
+    rownames(sorted) <- NULL
+    x <- head(sorted, n = 12)
+
+    png(file=paste(path2, "/Theta_",i,j , ".png", sep = ""),height = 25*nrow(x), width = 100*ncol(x))
     #png(file = paste(path,"table_for_graph",i,j, ".png" sep = ""), height=6, width=4)
     grid.table(x)
     dev.off()
@@ -167,17 +294,15 @@ x <- head(sorted, n = 12)
 
 
 
-
-
-
 ######################### Degree Plots
 
 #for (t in 1:N) {
-  path <-  "H:/Documents/MdS/Daten_der_Analyse/Distributions/Distributions_Final2"
+  #path <-  "H:/Documents/MdS/Daten_der_Analyse/Distributions/Distributions_Final2"
+  path <- "C:/Users/Arne/Dropbox/Masterarbeit/MdS/Daten_der_Analyse/Distributions/Distributions_JGL"
   #par(mfrow=c(length_var1,length_var2))
   maxMat <- matrix(0,1,9)
   for (r in 1:9) {
-    maxMat[r] <- max(degree(graphs_MdS[[r]]))
+    maxMat[r] <- max(degree(graphs_JGL[[r]]))
   }
 
   count <- 1
@@ -195,7 +320,7 @@ x <- head(sorted, n = 12)
       png(file=paste(path, "/Theta_",i,j , ".png", sep = ""))
       par(mar = c(5.1, 3, 4.1, 10))
       barplot(plotter[count,],
-              xlab = paste("G_", i,j, sep = ""), xlim = c(0, max(maxMat)),
+              xlab = paste("Theta_", i,j, sep = ""), xlim = c(0, max(maxMat)),
               ylim = c(0,0.5),  col = "skyblue")
       dev.off()
       count <- count + 1
@@ -203,162 +328,18 @@ x <- head(sorted, n = 12)
   }
 #}
 
-################################# For all Datasets (n = 32)
-
-  lambda1 <- c("0.5", "2", "5", "10")
-  lambda2 <- c("0.5", "2" ,"5", "10")
-
-
-   for (w in 1:2) {
-     for (l1 in lambda1) {
-       for (l2 in lambda2) {
-
-         ## Prepare the data
-         datapath <- paste("H:/Documents/MdS/Daten_der_Analyse/Daten/lambda1_",
-                           l1, "_lambda2_", l2,  "_weightmatrix_", w ,"_Gennetwork.RDS", sep = "")
-
-
-         Gen_network <- readRDS(datapath)
-         Adjs <- Gen_network$adj_matrices
-
-         graphs <- list()
-         subgraphs <- list()
-
-
-         for (m in 1:9) {
-           graphs[[m]] <- graph_from_adjacency_matrix(Adjs[[m]], mode= "undirected")
-           V(graphs[[m]])$name <- as.character(1:134)
-
-           nix <- which(degree(graphs[[m]]) != 0)
-           subgraphs[[m]] <- induced.subgraph(graphs[[m]],
-                                                   vids = nix)
-         }
-
-         ######################### Degree Plots
-
-
-         path <-  paste("H:/Documents/MdS/Daten_der_Analyse/Distributions/Distributions_MdS_", l1, "_", l2,  "_", "w", w , sep = "")
-
-         maxMat <- matrix(0,1,9)
-         for (r in 1:9) {
-           maxMat[r] <- max(degree(graphs[[r]]))
-         }
-
-         count <- 1
-         plotter <- matrix(0,9, max(maxMat)+1)
-         colnames(plotter) <- 0:max(maxMat)
-         for (i in 1:3) {
-           for(j in 1:3){
-             temp <- degree.distribution(graphs[[count]])
-             plotter[count,1:length(temp)] <- temp
-             names(plotter) <- 0:(length(plotter)-1)
-             test <- as.data.frame(plotter)
 
 
 
-             png(file=paste(path, "/Theta_",i,j , ".png", sep = ""))
-             par(mar = c(5.1, 3, 4.1, 10))
-             barplot(plotter[count,],
-                     xlab = paste("G_", i,j, sep = ""), xlim = c(0, max(maxMat)),
-                     ylim = c(0,0.5),  col = "skyblue")
-             dev.off()
-             count <- count + 1
-           }
-         }
-
-
-         ####################### plot tables
-         path2 <- paste("H:/Documents/MdS/Daten_der_Analyse/Tables/Tables_MdS_", l1, "_", l2,  "_", "w", w , sep = "")
-         counter <- 1
-         for (i in 1:3) {
-           for (j in 1:3) {
-             degrees <- degree(graphs[[counter]])
-             sorted <- as.data.frame(sort(degrees, decreasing = TRUE))
 
 
 
-             names(sorted) <- "Anzahl Knoten"
-             x <- head(sorted, n = 12)
-
-             png(file=paste(path2, "/Theta_",i,j , ".png", sep = ""))
-             #png(file = paste(path,"table_for_graph",i,j, ".png" sep = ""), height=6, width=4)
-             grid.table(x)
-             dev.off()
 
 
 
-             counter <- counter + 1
-           }
-         }
-
-
-         ######################### Graph Plots
-
-         path3 <- paste("H:/Documents/MdS/Daten_der_Analyse/Graphs/Graphs_MdS_", l1, "_", l2,  "_", "w", w , sep = "")
-         counter <- 1
-         for (i in 1:3) {
-           for (j in 1:3) {
-
-             #
-             # data <- toVisNetworkData(graphs_MdS[[counter]])
-             # data$nodes$label <- as.character(data$nodes$label)
-             # network1 <- visNetwork(nodes = data$nodes, edges = data$edges)%>%
-             #   #visIgraphLayout(layout = "layout_in_circle") %>%
-             #   #visIgraphLayout(layout = "layout_with_sugiyama") %>%
-             #   visOptions(highlightNearest = list(enabled = TRUE, degree = 1),
-             #              width = "100%", height = "100%")
-             #
-             # data2 <- toVisNetworkData(graphs_MdS_sub[[counter]])
-             # data2$nodes$label <- as.character(data2$nodes$label)
-             # network2 <- visNetwork(nodes = data2$nodes, edges = data2$edges)%>%
-             #   #visIgraphLayout(layout = "layout_in_circle") %>%
-             #   #visIgraphLayout(layout = "layout_with_sugiyama") %>%
-             #   visOptions(highlightNearest = list(enabled = TRUE, degree = 1),
-             #               width = "100%", height = "100%")
-             #
-             #
-             # visSave(network1, file = paste(path3, "/Theta_FULL_",i,j , ".html", sep = ""))
-             # visSave(network2, file = paste(path3, "/Theta_Subnetwork_",i,j , ".html", sep = ""))
-             g <- graphs[[counter]]
-             g2 <- subgraphs[[counter]]
-             max_deg <- max(degree(g)) # with a length of X vertices :
-             max_deg2 <- max(degree(g2))
-
-             if (max_deg < 80 ) {
-               colors <- (max_deg+20):20
-             } else if (max_deg >= 80){
-               x <- 100 - max_deg
-               colors <- 100:x
-             }
-
-
-             gray_colors <- as.data.frame(paste0("gray", colors))
-             colnames(gray_colors)  <- "farbe"
-
-             for (n in 1:length(V(g))) {
-               V(g)[n]$color <- gray_colors[degree(g)[n] + 1,]
-             }
-             for (n in 1:length(V(g2))) {
-               V(g2)[n]$color <- gray_colors[degree(g2)[n] + 1,]
-             }
-
-             png(file=paste(path3, "/Theta_",i,j , ".png", sep = ""), width= 960, height = 960, res = 120)
-             plot(g, layout=layout_with_fr, vertex.size=5)
-             dev.off()
-
-
-             png(file=paste(path3, "/Theta_SUB_",i,j , ".png", sep = ""), width= 960, height = 960, res = 120)
-             plot(g2, layout=layout_with_fr, vertex.size=5)
-             dev.off()
-
-
-             counter <- counter + 1
-           }
-         }
 
 
 
-       }
-     }
-   }
+
+
 
