d28fa8de1250ec3bc6d629d3032a1a9e57381d7a
godtheit <arne23@hotmail.de>
2022-08-04 13:12:52 +0200

    Simulation!

--- Analyse_Simulation/Hamming_Plots.R
+++ Analyse_Simulation/Hamming_Plots.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 setwd("H:/Documents/MdS/Analyse_Simulation/")
 source("get_rates.R")
 
@@ -6,73 +7,69 @@ source("get_rates.R")
 
 
 
-funfzig <- readRDS("test500.RDS")
-
-zwoelf <- readRDS("Test12.RDS")
-
-eins <- readRDS("TestMdS1.RDS")
-zwei <- readRDS("TestMdS2.RDS")
-drei <- readRDS("TestMdS3.RDS")
-vier <- readRDS("TestMdS4.RDS")
-
-load("alladjacs.RData")
-
-
-get_rates(BigListof_adjencies[[1]][[2]],zwoelf$adj_matrices[[4]])
-#TrueAdjs <- BigListof_adjencies[[1]]
-#sharedAdjs         <- eins$shared_adj
-#   zeros_shared <- matrix(0,67,67)
-#shared_adjacency <- rbind(cbind(zeros_shared, zeros_shared), cbind(sharedAdjs,zeros_shared)    )
-
-
-
-
-#JGL_est            <- Daten$placehodler3
-#Glasso_est         <- Daten$placehodler4
-
-
 MdS_list  <- JGL_list <- Glasso_list <- list()
+Results_as_frame <- c()
+Durchschnitte <- as.data.frame(matrix(0,320,7))
+colnames(Durchschnitte) <- c("Hamming-Distance", "Precision", "Recall", "Aic", "lambda1", "lambda2",  "id")
 
-for(k in 1:20){
-  Daten <- readRDS(paste0("H:/Documents/MdS/Cluster/Ergebnisse/",k, ".RDS")  )
+
+############## plotting MdS
+for(k in 1:320){
+  Daten <- readRDS(paste0("H:/Documents/MdS/Analyse_Simulation/MdS_Simulation/",k, ".RDS")  )
   TrueAdjs <- Daten$trueAdj
   MdS_est            <- Daten$adj_matrices
+
+  theta <- Daten$Theta
+  S <- Daten$S
   MdS  <- JGL <- Glasso <- as.data.frame(matrix(0,9,3))
   colnames(MdS) <- names(JGL) <- names(Glasso) <- c("Hamming-Distance", "Precision", "Recall")
+  MdS$Aic <- 0
+
   for (m in 1:9) {
 
+    MdS[m,1:3] <- get_rates(TrueAdjs[[m]], MdS_est[[m]])
 
-
-
-
-    MdS[m,] <- get_rates(TrueAdjs[[m]], MdS_est[[m]])
     #JGL[m,] <- get_rates(TrAdj, JGL_est[[m]])
     #Glasso[m,] <- get_rates(TrAdj, Glasso_est[[m]])
-  }
 
+    MdS$Aic <- mds_aic(Theta = Daten$Theta, S = Daten$S, n = Daten$n)
+  }
+ MdS$lambda1 <- Daten$lambda1
+ MdS$lambda2 <- Daten$lambda2
+ MdS$id <- k
+
+for (r in 1:7) {
+  Durchschnitte[k,r] <- mean(MdS[,r])
+}
+
+
+ Results_as_frame <- rbind(Results_as_frame, MdS)
   MdS_list[[k]] <- MdS
-  JGL_list[[k]] <- JGL
-  Glasso_list[[k]] <- Glasso
+
+
+
+  #JGL_list[[k]] <- JGL
+  #Glasso_list[[k]] <- Glasso
 }
 
-############ Start plotting ######################
+############## plots
+path <- "H:/Documents/MdS/Ergebnisse_Sim/"
+for (a in 11:14) {
+  for (b in c(1.5,2.25,3,3.75)) {
+    plotter <- subset(Results_as_frame, Results_as_frame$lambda1 == a & Results_as_frame$lambda2 == b)
+    plotter2 <- subset(Durchschnitte, Durchschnitte$lambda1 == a & Durchschnitte$lambda2 == b)
 
+    png(filename = paste0(path, "_lam1_", a ,"_lam2_", b, ".png"))
+    plot(y = plotter$Precision, x = plotter$Recall)
+    dev.off()
 
+    plot(y = plotter2$Precision, x = plotter2$Recall)
 
-for (k in 1:20) {
-  for (m in 1:10) {
-
-
-
-
-    if(m == 10)
-    {
-      #plot summe
-    }
   }
 }
 
 
-testmatrix1 <-matrix(sample(0:1,64,replace=TRUE),ncol=8)
-testmatrix2 <-matrix(sample(0:1,64,replace=TRUE),ncol=8)
+
+
+
 
